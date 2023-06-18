3166c10b3d1c4174ada1a862f3304b6aaa9e8290
Javier Lopez <javier.lopez.sola@gmail.com>
2023-06-18 23:35:00 +0200

    V0.3 - Simplificación web

--- js/navigation.js
+++ js/navigation.js
@@ -1,2 +1,11 @@
 
-  
+let body = document.body;
+  function modoDia(){ 
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+  }
+
+  function modoNoche(){
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+  }
